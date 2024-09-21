1bd5a3ebe674479600e9cbffe6b0a252d401d261
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2024-09-21 08:49:14 -0300

    alter: Guids Final

--- projetos/Guids/Program.cs
+++ projetos/Guids/Program.cs
@@ -7,8 +7,15 @@
             Console.WriteLine("Guids =>");
             Console.WriteLine("é usado muito para cria indentificador unicos");
 
+
             var id = Guid.NewGuid();
-            Console.WriteLine("\n" + id);
+            Console.WriteLine(id.GetType());
+
+            var id2 = id.ToString();
+            Console.WriteLine("\n" + id2.GetType());
+            Console.WriteLine(id2.Substring(0, 8));
+
+
         }
     }
 }
