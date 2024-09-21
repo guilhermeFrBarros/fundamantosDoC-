0cee0fdccb3426e8b36a3455fbb1c8460d7a925a
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2024-09-21 08:35:16 -0300

    feat: Guids

---
+++ projetos/Guids/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Guids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guids =>");
+            Console.WriteLine("é usado muito para cria indentificador unicos");
+
+            var id = Guid.NewGuid();
+            Console.WriteLine("\n" + id);
+        }
+    }
+}
