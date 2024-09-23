779b1b895df6ae62f7d3c2816b5634a48a0f5ba7
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2024-09-22 21:05:35 -0300

    feat: add StartWithEndsWith

---
+++ projetos/StringsResto/NewClass.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StringsResto;
+
+namespace StringsResto
+{
+    public class NewClass
+    {
+        // Sem modificador o padrão é privete
+        static void Main(string[] args)
+        {
+            StartWithEndsWith.StartWithEndsWithExec();
+
+        }
+    }
+}
---
+++ projetos/StringsResto/StartWithEndsWith.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StringsResto
+{
+    public class StartWithEndsWith
+    {
+        internal static void StartWithEndsWithExec()
+        {
+            var texto = "Este texto é um teste";
+            Console.WriteLine(texto.StartsWith("Este"));// True
+            Console.WriteLine(texto.StartsWith("este"));// False
+            Console.WriteLine(texto.StartsWith("este", StringComparison.OrdinalIgnoreCase));// True
+            Console.WriteLine(texto.StartsWith("texto"));// False
+
+            Console.WriteLine(texto.EndsWith("teste"));// True
+            Console.WriteLine(texto.EndsWith("Teste"));// False
+            Console.WriteLine(texto.EndsWith("xpox"));// False
+
+
+
+        }
+    }
+}
