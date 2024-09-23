90e6e6cfdfa096371124b417a55a3404ac84cfa9
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2024-09-22 21:09:55 -0300

    refactore: create namespace fundamentos

--- projetos/StringsResto/NewClass.cs
+++ projetos/StringsResto/NewClass.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using StringsResto;
-
+using fundamentos;
 namespace StringsResto
 {
     public class NewClass
